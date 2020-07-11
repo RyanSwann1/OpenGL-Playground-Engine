7e7e67358cdedbbf5a67dcf6704d70e59055c89d
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-11 12:08:26 +0100

    Quad rendered

---
+++ Globals.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include <array>
+
+namespace Globals
+{
+	constexpr unsigned int INVALID_OPENGL_ID = 0;
+
+	constexpr std::array<glm::vec2, 6> QUAD_COORDS = 
+	{
+		glm::vec2(-0.5f, -0.5f),
+		glm::vec2(0.5f, -0.5f),
+		glm::vec2(0.5f, 0.5f),
+		glm::vec2(0.5f, 0.5f),
+		glm::vec2(-0.5f, 0.5f),
+		glm::vec2(-0.5f, -0.5f)
+	};
+
+	constexpr std::array<glm::vec2, 6> TEXT_COORDS = 
+	{
+		glm::vec2(0.0f, 0.0f),
+		glm::vec2(1.0f, 0.0f),
+		glm::vec2(1.0f, 1.0f),
+		glm::vec2(1.0f, 1.0f),
+		glm::vec2(0.0f, 1.0f),
+		glm::vec2(0.0f, 0.0f)
+	};
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,72 @@
+#include "Globals.h"
+#include "SFML/Graphics.hpp"
+#include "glad.h"
+#include "ShaderHandler.h"
+#include <vector>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World\n";
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 4;
+	settings.majorVersion = 3;
+	settings.minorVersion = 3;
+	settings.attributeFlags = sf::ContextSettings::Core;
+	glm::uvec2 windowSize(640, 480);
+	sf::Window window(sf::VideoMode(windowSize.x, windowSize.y), "OpenGL Playground", sf::Style::Default, settings);
+	window.setFramerateLimit(60);
+	gladLoadGL();
+	glViewport(0, 0, windowSize.x, windowSize.y);
+
+	std::unique_ptr<ShaderHandler> shaderHandler = ShaderHandler::create();
+	assert(shaderHandler);
+	if (!shaderHandler)
+	{
+		std::cout << "Failed to load Shader Handler\n";
+		return -1;
+	}
+
+	shaderHandler->switchToShader(eShaderType::Default);
+
+	unsigned int vaoID = Globals::INVALID_OPENGL_ID;
+	glGenVertexArrays(1, &vaoID);
+	glBindVertexArray(vaoID);
+
+	std::vector<glm::vec2> positions;
+	positions.insert(positions.end(), Globals::QUAD_COORDS.begin(), Globals::QUAD_COORDS.end());
+	unsigned int positionsID = Globals::INVALID_OPENGL_ID;
+	glGenBuffers(1, &positionsID);
+	glBindBuffer(GL_ARRAY_BUFFER, positionsID);
+	glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(glm::vec2), positions.data(), GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (const void*)0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	while (window.isOpen())
+	{
+		sf::Event currentSFMLEvent;
+		while (window.pollEvent(currentSFMLEvent))
+		{
+			if (currentSFMLEvent.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBindVertexArray(vaoID);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		window.display();
+	}
+
+	glDeleteVertexArrays(1, &vaoID);
+	glDeleteBuffers(1, &positionsID);
+
 	return 0;
 }
