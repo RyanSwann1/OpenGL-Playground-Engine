6226dbd83deff57e37bdcc17148625960136c3df
Ryan Swann <RyanSwann1992@gmail.com>
2020-10-31 20:31:08 +0000

    Model refactor
    Now fits projects requirements

--- Model.cpp
+++ Model.cpp
@@ -5,44 +5,10 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtx/transform.hpp"
 
-Model::Model(bool renderFromCentrePosition, const glm::vec3& AABBSizeFromCenter, const glm::vec3& scale,
-	const std::string& fileName, std::vector<Mesh>&& meshes)
-	: modelName(fileName),
-	renderFromCentrePosition(renderFromCentrePosition),
-	AABBSizeFromCenter(AABBSizeFromCenter),
-	scale(scale),
-	meshes(std::move(meshes))
+Model::Model(std::vector<Mesh>&& meshes)
+	: meshes(std::move(meshes))
 {
 	attachMeshesToVAO();
-}
-
-void Model::render(ShaderHandler & shaderHandler, glm::vec3 entityPosition, bool entitySelected, const glm::vec3& rotation) const
-{
-	glm::mat4 model = glm::mat4(1.0f);
-	if (renderFromCentrePosition)
-	{
-		entityPosition.x += AABBSizeFromCenter.x;
-		entityPosition.z -= AABBSizeFromCenter.z;
-
-		model = glm::translate(model, entityPosition);
-		model = glm::scale(model, scale);
-		model = glm::translate(model, glm::vec3(-AABBSizeFromCenter.x, 0.0f, AABBSizeFromCenter.z));
-		model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::translate(model, glm::vec3(AABBSizeFromCenter.x, 0.0f, -AABBSizeFromCenter.z));
-	}
-	else
-	{
-		model = glm::translate(model, entityPosition);
-		model = glm::scale(model, scale);
-		model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
-	}
-	
-	shaderHandler.setUniformMat4f(eShaderType::Default, "uModel", model);
-
-	for (const auto& mesh : meshes)
-	{
-		mesh.render(shaderHandler, entitySelected);
-	}
 }
 
 void Model::attachMeshesToVAO() const
@@ -53,8 +19,7 @@ void Model::attachMeshesToVAO() const
 	}
 }
 
-std::unique_ptr<Model> Model::create(const std::string & fileName, bool renderFromCentrePosition, 
-	const glm::vec3& AABBSizeFromCenter, const glm::vec3& scale)
+std::unique_ptr<Model> Model::create(const std::string & fileName)
 {
 	std::vector<Mesh> meshes;
 	if (!ModelLoader::loadModel(fileName, meshes))
@@ -62,10 +27,20 @@ std::unique_ptr<Model> Model::create(const std::string & fileName, bool renderFr
 		return std::unique_ptr<Model>();
 	}
 
-	return std::unique_ptr<Model>(new Model(renderFromCentrePosition, AABBSizeFromCenter, scale, fileName, std::move(meshes)));
+	return std::unique_ptr<Model>(new Model(std::move(meshes)));
 }
 
-void Model::render(ShaderHandler& shaderHandler, const glm::vec3& position, glm::vec3 rotation) const
+void Model::render(ShaderHandler& shaderHandler, const glm::vec3& position, glm::vec3 rotation, glm::vec3 scale) const
 {	
-	render(shaderHandler, position, false, rotation);
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::translate(model, position);
+	model = glm::scale(model, scale);
+	model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	
+	shaderHandler.setUniformMat4f(eShaderType::Default, "uModel", model);
+
+	for (const auto& mesh : meshes)
+	{
+		mesh.render(shaderHandler);
+	}
 }
--- Model.h
+++ Model.h
@@ -10,22 +10,15 @@
 class ShaderHandler;
 struct Model : private NonMovable, private NonCopyable
 {
-	static std::unique_ptr<Model> create(const std::string& fileName, bool renderFromCentrePosition, 
-		const glm::vec3& AABBSizeFromCenter, const glm::vec3& scale);
+	static std::unique_ptr<Model> create(const std::string& fileName);
 
-	void render(ShaderHandler& shaderHandler, const glm::vec3& position, glm::vec3 rotation = glm::vec3()) const;
+	void render(ShaderHandler& shaderHandler, const glm::vec3& position, glm::vec3 rotation = glm::vec3(), 
+		glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f)) const;
 
-	const std::string modelName;
-	const bool renderFromCentrePosition;
-	const glm::vec3 AABBSizeFromCenter;
-	const glm::vec3 scale;
 	const std::vector<Mesh> meshes;
 	
 private:
-	Model(bool renderFromCentrePosition, const glm::vec3& sizeFromCentre, const glm::vec3& scale,
-		const std::string& fileName, std::vector<Mesh>&& meshes);
+	Model(std::vector<Mesh>&& meshes);
 
-	void render(ShaderHandler& shaderHandler, glm::vec3 entityPosition, bool entitySelected, 
-		const glm::vec3& rotation) const;
 	void attachMeshesToVAO() const;
 };
