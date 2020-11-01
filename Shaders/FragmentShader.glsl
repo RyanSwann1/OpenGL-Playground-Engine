f6aad61592c6503ccaeae7451aa62a0e1a2fa602
Ryan Swann <RyanSwann1992@gmail.com>
2020-11-01 15:42:39 +0000

    Implemented initial ambient light

--- Shaders/FragmentShader.glsl
+++ Shaders/FragmentShader.glsl
@@ -12,16 +12,18 @@ uniform bool uDiffuseTexture;
 uniform bool uSpecularTexture;
 uniform vec3 uMaterialColor;
 
+const float ambientStrength = 0.5;
+
 void main()
 {
 	if(uDiffuseTexture)
 	{
-		color = texture(texture_diffuse, vTextCoords);
+		color = texture(texture_diffuse, vTextCoords) * ambientStrength;
 	}
 
 	if(uSpecularTexture)
 	{
-		color = texture(texture_specular, vTextCoords);
+		color = texture(texture_specular, vTextCoords) * ambientStrength;
 	}
 
 	if(!uDiffuseTexture && !uSpecularTexture)
@@ -29,6 +31,6 @@ void main()
 		float dotFactor = dot(vNormal, vec3(0.0, 1.0, 0.0)) * 0.5 + 0.5;
 		float darkenFactor = 0.5 + dotFactor * (1.0 - 0.5);
 		vec3 outputColour = uMaterialColor * darkenFactor;
-		color = vec4(outputColour, 1.0);
+		color = vec4(outputColour * ambientStrength, 1.0);
 	}
 };
