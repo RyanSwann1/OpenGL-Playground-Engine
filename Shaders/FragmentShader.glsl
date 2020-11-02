46283552a01da4485e842da5c4fa985a39b39223
Ryan Swann <RyanSwann1992@gmail.com>
2020-11-02 19:47:40 +0000

    Implemented initial specular lighting

--- Shaders/FragmentShader.glsl
+++ Shaders/FragmentShader.glsl
@@ -14,25 +14,33 @@ uniform bool uSpecularTexture;
 uniform vec3 uMaterialColor;
 
 uniform vec3 uLightPosition;
+uniform vec3 uLightColor;
 
-const vec3 lightColor = vec3(1, 1, 0);
+uniform vec3 uViewPosition;
+
 const float ambientStrength = 0.7;
+const float specularStrength = 0.5;
 
 void main()
 {
 	//Diffuse Lighting
 	vec3 n = normalize(vNormal);
 	vec3 lightDirection = normalize(uLightPosition - vFragPosition);
-	vec3 diffuse = max(dot(n, lightDirection), 0.0) * lightColor;
+	vec3 diffuse = max(dot(lightDirection, n), 0.0) * uLightColor;
+
+	//Specular Lighting
+	vec3 viewDirection = normalize(uViewPosition - vFragPosition);
+	vec3 reflectDirection = reflect(-lightDirection, n);
+	vec3 specular = uLightColor * specularStrength * (pow(max(dot(viewDirection, reflectDirection), 0.0), 128));
 
 	if(uDiffuseTexture)
 	{
-		color = texture(texture_diffuse, vTextCoords) * ambientStrength;
+		color = texture(texture_diffuse, vTextCoords);
 	}
 
 	if(uSpecularTexture)
 	{
-		color = texture(texture_specular, vTextCoords) * ambientStrength;
+		color = texture(texture_specular, vTextCoords);
 	}
 
 	if(!uDiffuseTexture && !uSpecularTexture)
@@ -43,5 +51,5 @@ void main()
 		color = vec4(outputColour * ambientStrength, 1.0);
 	}
 
-	color = vec4(vec3((ambientStrength + diffuse) * color.xyz), 1.0);
+	color = vec4(vec3((ambientStrength + diffuse + specular) * color.xyz), 1.0);	
 };
