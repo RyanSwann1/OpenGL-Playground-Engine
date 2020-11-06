4a113fbb21a6eeba45b2105a774884fbb2758314
Ryan Swann <RyanSwann1992@gmail.com>
2020-11-06 10:07:40 +0000

    loadTexture now shared function - no longer duplicating functionality

--- Texture.h
+++ Texture.h
@@ -8,6 +8,7 @@
 struct Texture : private NonCopyable, private NonMovable
 {
 	static std::unique_ptr<Texture> create(const std::string& path, const std::string& directory, const std::string& type);
+	static std::unique_ptr<Texture> create(const std::string& fileName, const std::string& type);
 	~Texture();
 
 	void bind() const;
@@ -18,4 +19,5 @@ struct Texture : private NonCopyable, private NonMovable
 
 private:
 	Texture(unsigned int ID, const std::string& type, const std::string& path);
+	Texture(unsigned int ID, const std::string& type);
 };
